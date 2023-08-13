eb8c1b58347375c6b9780f38224617f7042bad68
Tim <tim@brightsidedeveloper.com>
2023-08-13 13:39:04 -0500

    Form Working

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,16 +1,46 @@
 'use client'
 
+import * as z from 'zod'
 import { Button } from '@/components/ui/button'
 import Heading from '@/components/ui/heading'
 import { Separator } from '@/components/ui/separator'
 import { Store } from '@prisma/client'
 import { Trash } from 'lucide-react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState } from 'react'
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 
 interface SettingFormProps {
   initialData: Store
 }
 
+const formSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+})
+
+type SettingFormValues = z.infer<typeof formSchema>
+
 export default function SettingsForm({ initialData }: SettingFormProps) {
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const form = useForm<SettingFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  })
+
+  const onSubmit = async (values: SettingFormValues) => {
+    console.log(values)
+  }
+
   return (
     <>
       <div className='flex items-center justify-between'>
@@ -20,6 +50,35 @@ export default function SettingsForm({ initialData }: SettingFormProps) {
         </Button>
       </div>
       <Separator />
+      <FormProvider {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='space-y-8 w-full'
+        >
+          <div className='grid grid-cols-3 gap-8'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder='Store name'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className='ml-auto' type='submit'>
+            Save Changes
+          </Button>
+        </form>
+      </FormProvider>
     </>
   )
 }
--- components/modals/StoreModal.tsx
+++ components/modals/StoreModal.tsx
@@ -20,7 +20,7 @@ import axios from 'axios'
 import { toast } from 'react-hot-toast'
 
 const formSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(1, 'Name is required'),
 })
 
 export const StoreModal = () => {
